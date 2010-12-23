5904a178466d91fa7c5e05d67f165f965f1c9268
Laurent Gilles <laurent.gilles@codegist.org>
2010-12-23 12:08:40 +0000

    added InputStreamWrapper class

---
+++ src/main/java/org/codegist/common/io/InputStreamWrapper.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2010 CodeGist.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ===================================================================
+ *
+ * More information at http://www.codegist.org.
+ */
+
+package org.codegist.common.io;
+
+import org.codegist.common.lang.EqualsBuilder;
+import org.codegist.common.lang.Validate;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Simple input stream wrapper that delegates all methods calls to the underlying input stream.
+ */
+public class InputStreamWrapper extends InputStream {
+
+    private final InputStream delegate;
+
+    public InputStreamWrapper(InputStream delegate) {
+        Validate.notNull(delegate, "Input stream can't be null");
+        this.delegate = delegate;
+    }
+
+    @Override
+    public int read() throws IOException {
+        return delegate.read();
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        return delegate.read(b);
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        return delegate.read(b,off,len);
+    }
+
+    @Override
+    public long skip(long n) throws IOException {
+        return delegate.skip(n);
+    }
+
+    @Override
+    public int available() throws IOException {
+        return delegate.available();
+    }
+
+    @Override
+    public void close() throws IOException {
+        delegate.close();
+    }
+
+    @Override
+    public void mark(int readlimit) {
+        delegate.mark(readlimit);
+    }
+
+    @Override
+    public void reset() throws IOException {
+        delegate.reset();
+    }
+
+    @Override
+    public boolean markSupported() {
+        return delegate.markSupported();
+    }
+
+    @Override
+    public int hashCode() {
+        return delegate.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        return new EqualsBuilder()
+                .append(delegate, ((InputStreamWrapper) o).delegate)
+                .equals();
+    }
+
+    @Override
+    public String toString() {
+        return delegate.toString();
+    }
+
+}
