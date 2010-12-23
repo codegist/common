46bb0a5219558c87a6dab75883695403b0290bb3
Laurent Gilles <laurent.gilles@codegist.org>
2010-12-23 17:45:23 +0000

    fixed broken test

--- src/test/java/org/codegist/common/codec/HexTest.java
+++ src/test/java/org/codegist/common/codec/HexTest.java
@@ -36,17 +36,17 @@ public class HexTest {
 
     @Test
     public void testEmpty() {
-        assertEquals("", Hex.encode(new byte[0]));
+        assertEquals("", Hex.encodeAsString(new byte[0]));
     }
 
     @Test
     public void test1() {
-        assertEquals("000102030405060708090a0b0c0d0e0f1011", Hex.encode(new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17}));
+        assertEquals("000102030405060708090a0b0c0d0e0f1011", Hex.encodeAsString(new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17}));
     }
 
     @Test
     public void test2() {
-        assertEquals("00fffefdfcfbfaf9f8f7f6f5f4f3f2f1f0ef", Hex.encode(new byte[]{0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, -17}));
+        assertEquals("00fffefdfcfbfaf9f8f7f6f5f4f3f2f1f0ef", Hex.encodeAsString(new byte[]{0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, -17}));
     }
 
 
