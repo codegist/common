1e5efabff51dbe0cf8a8e4204df15a52e4b3129b
CodeGist <laurent.gilles@codegist.org>
2011-01-07 14:23:44 +0000

    import clean up

--- src/main/java/org/codegist/common/reflect/ParameterizedTypeImpl.java
+++ src/main/java/org/codegist/common/reflect/ParameterizedTypeImpl.java
@@ -45,13 +45,7 @@
 
 package org.codegist.common.reflect;
 
-
-
-
-import sun.reflect.generics.tree.FieldTypeSignature;
-
 import java.lang.reflect.MalformedParameterizedTypeException;
-import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
